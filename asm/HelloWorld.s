4903f543579898a68581c7cde3921cf29268141c
root <j.j.kan1948@gmail.com>
2025-10-19 23:41:29 +0900

    feat: make shellcode injection

---
+++ asm/HelloWorld.s
@@ -0,0 +1,13 @@
+section .text
+global _start
+	msg db 'Hello World','$';
+	len equ $ -msg
+_start:
+	mov edx, len
+	mov ecx, msg
+	mov ebx, 1
+	mov eax, 4
+	int 0x80
+	mov ebx,0
+	mov eax,1
+	int 0x80
