8aa5e0e7e97fae8b0960a26ae0485915c762a56b
insub <j.j.kant1948@gmail.com>
2025-11-17 21:08:46 +0900

    add main

---
+++ elf_packer/sources/main.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: insub <insub@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/17 20:51:01 by insub             #+#    #+#             */
+/*   Updated: 2025/11/17 21:07:58 by insub            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "main.h"
+
+int main(int argc, char *argv[])
+{
+    // if (argc != 2) return print_error(WRONG_ARGS, ERRNO_FALSE);
+    
+    // open file
+    FILE *fp = NULL;
+    // if (fp = fopen(argv[1], "rb+")) return print_error(WRONG_ARGS, ERRNO_TRUE);
+    
+    // read file
+    
+    // parse elf
+
+    // calculate padding
+    // concaternate padding, stub to copy 
+    // set stub jmp address to entry_point
+    // change entry_point to stub
+    
+    // write packed exe
+
+    return 0;
+}
